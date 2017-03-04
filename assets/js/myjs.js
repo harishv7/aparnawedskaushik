c0515442d6428b5801ee5a10634f9c13d8721b9f
Harish V <harishv207@gmail.com>
2017-03-04 23:30:43 +0800

    fix audio video collide

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -94,7 +94,7 @@ $(document).ready(function() {
 	}
 	window.onload = loadBg();
 
-	var bg_loop = document.getElementById("bg-loop");
+//	var bg_loop = document.getElementById("bg-loop");
 
 	/* Controls for Bg Loop Music */
 	$("#bg-loop-btn2").click(function() {
@@ -112,50 +112,4 @@ $(document).ready(function() {
 			bg_loop.pause();
 		}
 	});
-
-	$("#welcome-video").click(function() {
-		bg_loop.pause();
-	});
-
-	// Welcome video    
-	// 2. This code loads the IFrame Player API code asynchronously.
-	var tag = document.createElement('script');
-
-	tag.src = "https://www.youtube.com/iframe_api";
-	var firstScriptTag = document.getElementsByTagName('script')[0];
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-	// 3. This function creates an <iframe> (and YouTube player)
-	//    after the API code downloads.
-	var player;
-	function onYouTubeIframeAPIReady() {
-		player = new YT.Player('player', {
-			height: '390',
-			width: '640',
-			videoId: 'AtIk06Y_BAs',
-			events: {
-				'onReady': onPlayerReady,
-				'onStateChange': onPlayerStateChange
-			}
-		});
-	}
-
-	// 4. The API will call this function when the video player is ready.
-	function onPlayerReady(event) {
-
-	}
-
-	// 5. The API calls this function when the player's state changes.
-	//    The function indicates that when playing a video (state=1),
-	//    the player should play for six seconds and then stop.
-	var done = false;
-	function onPlayerStateChange(event) {
-		if(!bg_loop.paused) {
-			bg_loop.pause();
-		}
-	}
-
-	function stopVideo() {
-		player.stopVideo();
-	}
 });
